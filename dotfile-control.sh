046b2922cb8bfae9f56a85450e029660ce3c8329
Yonatan Laurence <yangning@bu.edu>
2015-01-18 16:08:02 -0500

    move logs to logfile to prevent clutter

--- dotfile-control.sh
+++ dotfile-control.sh
@@ -2,31 +2,34 @@
 DOTFILES_DIR=.dotfiles
 SETUP_FILE=setupfiles.txt
 TRACKING_FILE=tracking.log
+LOG_DIR=logs
 CURRENT_DIR="$(dirname $0)"
 TODAY=$(date +"%Y-%m-%d")
 LOG_FORMAT="%Y-%m-%d %H:%M:%S"
-BACKUP_DIR=".ybackup-$TODAY"
+BACKUP_DIR=".ybackup"
+NEW_BACKUP_DIR="$BACKUP_DIR-$TODAY"
 INSTANCE="$USER@`hostname -f`"
 
 logit(){
-    echo "[$INSTANCE][`date +"$LOG_FORMAT"`] $1" >> $TRACKING_FILE
+    echo "[$INSTANCE][`date +"$LOG_FORMAT"`] $1" >> "$LOG_DIR/$TRACKING_FILE"
 }
 showit(){
     logit "$1"
     echo "[`basename $0`] $1"
 }
 if [ "$1" = "setup" ]; then
+    # Remove any previous attempts to install
+    cd $HOME
+    # rm -rf $DOTFILES_DIR
+    find . -maxdepth 1 -name "$BACKUP_DIR*" | xargs rm -rf
     # Clone this git repo
     showit "Cloning 'dotfiles' repo..."
-    rm -rf "$HOME/$DOTFILES_DIR"
-    git clone https://github.com/yangningBU/dotfiles.git "$HOME/$DOTFILES_DIR"
+    git clone https://github.com/yangningBU/dotfiles.git $DOTFILES_DIR
     showit "Backing up current settings..."
-    
-    
     # Create a backup directory to hide all of the old files and prevent clutter
-    if [ ! -d $HOME/$BACKUP_DIR ]; then
-        showit "Creating new backup directory called $BACKUP_DIR ..."
-        mkdir $HOME/$BACKUP_DIR
+    if [ ! -d $NEW_BACKUP_DIR ]; then
+        showit "Creating new backup directory called $NEW_BACKUP_DIR ..."
+        mkdir $NEW_BACKUP_DIR
     else
         showit "Using current backup directory from today..."
     fi
@@ -39,19 +42,24 @@ if [ "$1" = "setup" ]; then
     do
         if [ -e $_sourceName ]; then
             showit "Moving $_sourceName ..."
-            mv $HOME/$_sourceName $HOME/$BACKUP_DIR/
+            mv $_sourceName $NEW_BACKUP_DIR/
         fi
         showit "Setting up symlink $_linkName for $_sourceName"
         ln -s $CURRENT_DIR/$_sourceName $HOME/$_linkName
     done < "$CURRENT_DIR/$SETUP_FILE"
 
     showit "Installing Vundle"
-    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    
+    if [ -d $HOME/.vim/bundle ]; then
+        logit ".vim/bundle doesn't exist, creating a new one"
+        mkdir -p .vim/bundle
+    fi
+    git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+
     logit "Pushing log changes to origin/master..."
-    cd $DOTFILES_DIR
+    cd $DOTFILES_DIR/$LOG_DIR
     mv tracking.log tracking.log.$TODAY.$INSTANCE
-    "find . -name \"tracking.log*\" | xargs git add"
+    find . -name "tracking.log*" | xargs git add
     git commit -m "logging new setup"
     showit "Setup complete."
 elif [ "$1" = "breakdown" ]; then
