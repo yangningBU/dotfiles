7ce92d6932546df880c49238f0b9841190ee253f
Yonatan Laurence <yangning@bu.edu>
2015-01-18 10:53:47 -0500

    Renamed setup to dotfile-control and added [setup|breakdown] options

---
+++ dotfile-control.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+DOTFILES_DIR=.dotfiles
+SETUP_FILE=setupfiles.txt
+TRACKING_FILE=tracking.log
+CURRENT_DIR="$(dirname $0)"
+TODAY=$(date +"%Y-%m-%d")
+LOG_FORMAT="%Y-%m-%d %H:%M:%S"
+BACKUP_DIR=".ybackup-$TODAY"
+
+logit(){
+    echo "[$USER@`hostname -f`][`date +"$LOG_FORMAT"`] $1" >> $TRACKING_FILE
+}
+showit(){
+    logit "$1"
+    echo $1
+}
+if [ "$1" = "setup" ]; then
+    # Clone this git repo
+    showit "Cloning 'dotfiles' repo..."
+    echo rm -rf "$HOME/$DOTFILES_DIR"
+    echo git clone https://github.com/yangningBU/dotfiles.git "$HOME/$DOTFILES_DIR"
+    showit "Backing up current settings..."
+    
+    
+    # Create a backup directory to hide all of the old files and prevent clutter
+    if [ ! -d $HOME/$BACKUP_DIR ]; then
+        showit "Creating new backup directory called $BACKUP_DIR ..."
+        echo mkdir $HOME/$BACKUP_DIR
+    else
+        showit "Using current backup directory from today..."
+    fi
+    
+    # The setup file currently has each line entry with two values, 
+    # the name of what the file is called in my repository (_sourceName)
+    # followed by the name of the target file in the home directory (_linkName)
+    # which are space delimited
+    while read _sourceName _linkName
+    do
+        if [ -e $_sourceName ]; then
+            showit "Moving $_sourcename..."
+            echo mv $HOME/$_sourceName $HOME/$BACKUP_DIR/
+        fi
+        showit "Setting up symlink $_linkName for $_sourceName"
+        echo ln -s $CURRENT_DIR/$_sourceName $HOME/$_linkName
+    done < "$CURRENT_DIR/$SETUP_FILE"
+    
+    showit "Setup complete."
+elif [ "$1" = "breakdown" ]; then
+    showit "Preparing to remove yangningBU/dotfile changes on `hostname -f` for $USER"
+else
+    echo "Hey, you missed an input parameter. No worries, here's an example:"
+    printf "\t./dotfile-control.sh [setup|breakdown]\n"
+fi
